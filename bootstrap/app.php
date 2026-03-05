dba2857c5b742ed74493e3c1f17bc1904ee4c6bb
eason.lee <eason.lee@104.com.tw>
2026-03-05 13:50:31 +0800

    feat: 因應 CI 調整架構

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->api(prepend: [
+            \Illuminate\Cookie\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
