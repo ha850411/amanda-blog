718244d4a671ce4fa2ebc8f569a804dd5ec68ff5
eason.lee <eason.lee@104.com.tw>
2026-03-09 11:07:33 +0800

    fix: 修正 http mixed 問題

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,6 +20,7 @@ return Application::configure(basePath: dirname(__DIR__))
         },
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->api(prepend: [
             \Illuminate\Cookie\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
